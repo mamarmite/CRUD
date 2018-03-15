fe516769817383057091279ab90a97963537318e
Marc-André Martin <support@mamarmite.com>
2018-03-15 10:09:02 -0400

    When this is used on checklist, keep showed field if it's associated checkfield is checked. #Need-refactoring : to make it easier to read.

--- src/public/crud/js/crud-field-trait-toggable.js
+++ src/public/crud/js/crud-field-trait-toggable.js
@@ -16,19 +16,26 @@
         var defaults = {
                 dataName: "field-toggles",
             },
-            settings = $.extend( {}, defaults, options ),
-            hiddenFields    = [];
+            settings            = $.extend( {}, defaults, options ),
+            hiddenFields        = [],
+            _allfields          = [];
+
+
 
         return this.each(function(){
 
             //setup based properties. @todo : make global vars more viewable with g_ ? or find an ES6 convention ready.
-            var $self           = $(this),
-                toggles         = $self.data(settings.dataName),
-                value           = $self.val(),
-                fieldSet        = $self.attr('name');
+            var $self                   = $(this),
+                toggles                 = $self.data(settings.dataName),
+                value                   = $self.val(),
+                fieldSet                = $self.attr('name');
 
-            this.init           = init;
-            this.toggle         = toggle;
+            this.init                   = init;
+            this.toggle                 = toggle;
+            this.haveCheckedField       = haveCheckedField;
+            this.isValueChecked         = isValueChecked;
+
+            _allfields.push($self);
 
             function init() {
 
@@ -41,31 +48,32 @@
                     //use general jquery each method, because it's missed in js I think.
                     $.each( toggles, function(idx, obj)
                     {
-                        var fields = obj,//this.toggles[idx],
-                            target_showWhenIds = "",
-                            target_showWhenParams = "";
-
+                        var fields = toggles[idx],
+                            target_showWhenIds = "";
                         //loop through index here ?
                         $.each(fields, function(name, params)
                         {
-                            var f = $('[name="'+name+'"]');
+                            var f;
+                            if (typeof name === 'number') {
+                                //- if it's a normal array with no params.
+                                f = $('[name="'+params+'"]');
+                            } else {
+                                f = $('[name="'+name+'"]');
+                            }
                             if( f.length )
                             {
                                 hiddenFields[ fieldSet ].push(f);
                                 target_showWhenIds = f.data("showWhen") ? f.data("showWhen")+","+idx : idx;//the id or id's list
-                                target_showWhenParams = f.data("showWhenParams") ? f.data("showWhenParams")+","+params : params;
-                                
-                                f.data("showWhen",target_showWhenIds);//Set the main objet on every field
-                                f.data("field-toggles", this.toggles);//Set the main objet on every field
-                                f.data("sync",0);
+
+                                f.data("showWhen", target_showWhenIds);//Set the main objet on every field
                                 f.parents('.form-group').hide();
+
                             }
                             lastFieldName = name;
                         });
                     });
                 }
                 this.toggle();
-
             };//end of init function.
 
             /**
@@ -74,9 +82,11 @@
             function toggle() {
 
                 //setup based properties. @todo : make global vars more viewable with g_ ? or find an ES6 convention ready.
-                var $self           = $(this),
+                var self            = this,
+                    $self           = $(this),
                     value           = $self.val(),
-                    fieldSet        = $self.attr('name');
+                    fieldSet        = $self.attr('name'),
+                    checked         = $self.is(":checked");
 
                 // if the value is found in the toggles array, we passed through it.
                 if ( typeof toggles[ value ] !== "undefined" ) {
@@ -84,24 +94,32 @@
                     //loop through the showWhen array (it's always)
                     $.each(hiddenFields[ fieldSet ], function(idx, targetField)
                     {
-                        targetFieldShowWhen = targetField.data('showWhen').split(",");
                         
-                        /// if the value is in array.
-                        if (targetFieldShowWhen[targetFieldShowWhen.indexOf(value)] == value)
+                        targetFieldShowWhen = targetField.data('show-when');
+                        targetValues = targetFieldShowWhen.split(",");
+                        targetFieldValue = targetValues[targetValues.indexOf(value)];
+                        
+                        /// if the value is in array and the value is true (for checkbox)
+                        if ( targetFieldValue == value && 
+                            ($self.is(":checkbox") || $self.is(":radio") ? $self.is(":checked") : true ) )
                         {
                             targetField.parents('.form-group').show();
-                            targetField.data("sync", value);
                         }
                         else
                         {
-                            targetField.parents('.form-group').hide();
+                            //toggles[value][0] === targetField.attr("name") && 
+                            //console.log("top",toggles[value][0], "===", targetField.attr("name"));
+                            //if its a check box, check if there is other checkbox check. @todo check if it's associated.
+                            if (toggles[value][0] === targetField.attr("name") || ($self.is(":checkbox") || $self.is(":radio") ? !self.haveCheckedField() : true ) ) {
+                                targetField.parents('.form-group').hide();
+                            }
                         }
 
                         targetLabel = toggles[value][targetField.attr("name")];
+
                         //change the label
-                        if (targetLabel)
+                        if (targetLabel && targetLabel["label"])
                         {
-                            //@todo : check if Label exist.
                             targetField.siblings("label").text(targetLabel["label"]);
                         }
                     });
@@ -109,14 +127,50 @@
                 }
                 else
                 {
+
                     //hide all field if we are not set.
                     $.each(hiddenFields[ fieldSet ], function(idx, targetField)
                     {
-                        targetField.parents('.form-group').hide();
+                        targetFieldShowWhen = targetField.data('show-when');
+                        //  That is a sign that my algorith got flaws. Recheck your structure for checks.
+                        if (!$('[name="'+fieldSet+'"][value='+targetFieldShowWhen+']').is(":checked"))
+                        {
+                            targetField.parents('.form-group').hide();
+                        }
                     });
                 }
 
             };//end of toggle function.
+
+            function haveCheckedField(value) {
+                var checked_fields = [],
+                    value = value ? value : null;
+                $.each(_allfields, function(key, field) {
+                    
+                    if (field.val() == value) {
+                        //console.log(field.attr("name"),field.is(":checked"), field.val(), "==", value);
+                    }
+                    if (field.is(":checked")) {
+                        checked_fields.push(field);
+                    }
+                });
+                //console.log(checked_fields.length);
+                return checked_fields.length > 0;
+            }
+
+            function isToggleFieldChecked(value) {
+
+            }
+
+            function isValueChecked(value) {
+
+                $.each(_allfields, function(key, field) {
+                    if (field.is(":checked")) {
+                        if (field.val() == value) return true;
+                    }
+                });
+                return false;
+            }
 
             // Target element with class.
             $self.on('change', function() {
